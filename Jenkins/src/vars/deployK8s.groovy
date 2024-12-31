282c012bde80eba2b5075430b30ee93cf609228c
MariamWafik349 <mariamwafik10@gmail.com>
2024-12-31 13:20:49 +0200

    Create deployK8s.groovy

---
+++ Jenkins/src/vars/deployK8s.groovy
@@ -0,0 +1,13 @@
+def call(String DEPLOYMENT_FILE, String SERVICE_FILE) {
+    sh '''
+    export KUBECONFIG=~/.kube/config
+    # Check if the file exists
+            ls -l ${DEPLOYMENT_FILE}
+            ls -l ${SERVICE_FILE}
+            
+    echo "Applying Kubernetes deployment file: ${DEPLOYMENT_FILE}"
+    kubectl apply -f ${DEPLOYMENT_FILE}
+    echo "Applying Kubernetes deployment file: ${SERVICE_FILE}"
+    kubectl apply -f ${SERVICE_FILE}
+    '''
+}
